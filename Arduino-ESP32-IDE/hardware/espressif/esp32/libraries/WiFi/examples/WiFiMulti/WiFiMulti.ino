29253330c3b7d8adcbae6a8cc2449ac2efbc182c
Lily <lily@lilygo.cc>
2017-12-17 12:54:55 +0800

    first commit

---
+++ Arduino-ESP32-IDE/hardware/espressif/esp32/libraries/WiFi/examples/WiFiMulti/WiFiMulti.ino
@@ -0,0 +1,33 @@
+/*
+ *  This sketch trys to Connect to the best AP based on a given list
+ *
+ */
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+
+WiFiMulti wifiMulti;
+
+void setup()
+{
+    Serial.begin(115200);
+    delay(10);
+
+    wifiMulti.addAP("MasterHax_2.4G", "wittyercheese551");
+
+    Serial.println("Connecting Wifi...");
+    if(wifiMulti.run() == WL_CONNECTED) {
+        Serial.println("");
+        Serial.println("WiFi connected");
+        Serial.println("IP address: ");
+        Serial.println(WiFi.localIP());
+    }
+}
+
+void loop()
+{
+    if(wifiMulti.run() != WL_CONNECTED) {
+        Serial.println("WiFi not connected!");
+        delay(1000);
+    }
+}
